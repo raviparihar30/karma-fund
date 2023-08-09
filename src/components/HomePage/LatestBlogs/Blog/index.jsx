ef469199ea5a45bfa14c786bdf375d145719e943
nayan <sontiya.nayan@gmail.com>
2023-08-10 02:03:58 +0530

    listing change

--- src/components/HomePage/LatestBlogs/Blog/index.jsx
+++ src/components/HomePage/LatestBlogs/Blog/index.jsx
@@ -1,11 +1,36 @@
+import React from "react";
 import { useNavigate } from "react-router-dom";
+import dayjs from "dayjs"; // Import dayjs
+import relativeTime from "dayjs/plugin/relativeTime"; // Import the relativeTime plugin
 import "./index.m.css";
 
-const Blog = ({ title, image, id }) => {
+// Extend dayjs with the relativeTime plugin
+dayjs.extend(relativeTime);
+
+const Blog = ({ title, subTitle, image, author, updatedAt, id, likes }) => {
   const navigate = useNavigate();
+
+  // Format the date using dayjs
+  const formattedDate = dayjs(updatedAt).format("MMMM D, YYYY");
+
   return (
-    <div class="card blog-card" onClick={() => navigate(`/blog/${id}`)}>
-      <img class="card-img-top mw-100 h-100" src={image} alt="Card image cap" />
+    <div className="blog-card" onClick={() => navigate(`/blog/${id}`)}>
+      <div className="blog-image-container">
+        <img className="blog-image" src={image} alt="Blog" />
+      </div>
+      <div className="blog-content">
+        <div className="blog-text">
+          <h3 className="blog-title-main">{title}</h3>
+          <p className="blog-subtitle-main">{subTitle}</p>
+          <div className="blog-author-date">
+            <div className="blog-author-date-inner">
+              <div className="blog-author">{"Mr. Mahendra"}</div>
+              <div className="blog-date">{formattedDate}</div>
+            </div>
+            <div className="blog-like">❤️ {likes}</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/HomePage/LatestBlogs/index.jsx
+++ src/components/HomePage/LatestBlogs/index.jsx
@@ -1,13 +1,12 @@
-import "./index.m.css";
-import blogs from "./blogs.json";
-import { Button } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Button, Col, Row } from "react-bootstrap";
 import Blog from "./Blog";
-import { useEffect, useState } from "react";
 import { imageUrl } from "../../../apis";
 import { Link } from "react-router-dom";
 
 const LatestBlogs = () => {
   const [latestPosts, setLatestPosts] = useState([]);
+
   const fetchLatestPosts = async () => {
     const response = await fetch(
       "https://4f1c-182-70-202-68.ngrok-free.app/api/posts/"
@@ -15,12 +14,14 @@ const LatestBlogs = () => {
     if (response.ok) {
       const { data } = await response.json();
       setLatestPosts(
-        data.map(({ title, image, id }) => ({
+        data.map(({ title, image, id, subTitle, updatedAt }) => ({
           image: image
             ? `${imageUrl}${image}`
             : "https://cdn.pixabay.com/photo/2023/03/23/05/37/bird-7871182_1280.jpg",
           title,
           id,
+          subTitle,
+          updatedAt,
         }))
       );
     } else {
@@ -31,47 +32,17 @@ const LatestBlogs = () => {
   useEffect(() => {
     fetchLatestPosts();
   }, []);
-
+  console.log("latestPosts  => ", latestPosts);
   return (
     <div className="container my-5 blog-container">
-      <h1 className="mb-5">Latest inspiration</h1>
-      <div className="d-flex flex-wrap gap-4">
-        <Button
-          variant="outline-primary"
-          className="px-4 py-2 rounded-pill fw-bold border-primary"
-        >
-          View All
-        </Button>
-        <Button
-          variant="outline-secondary"
-          className="px-4 py-2 rounded-pill fw-bold border-secondary"
-        >
-          Illustration
-        </Button>
-        <Button
-          variant="outline-secondary"
-          className="px-4 py-2 rounded-pill fw-bold border-secondary"
-        >
-          Branding
-        </Button>
-        <Button
-          variant="outline-secondary"
-          className="px-4 py-2 rounded-pill fw-bold border-secondary"
-        >
-          Product Designes
-        </Button>
-        <Button
-          variant="outline-secondary"
-          className="px-4 py-2 rounded-pill fw-bold border-secondary"
-        >
-          Typography
-        </Button>
-      </div>
-      <div className="d-flex flex-wrap justify-content-center gap-4 my-5 bg-light py-5 rounded">
+      <h1 className="mb-5">Latest Blogs</h1>
+      <Row className="my-5 bg-light py-5 rounded">
         {latestPosts.map((blog, index) => (
-          <Blog {...blog} key={index} />
+          <Col key={index} md={6} className="mb-4">
+            <Blog {...blog} />
+          </Col>
         ))}
-      </div>
+      </Row>
       <Button
         variant="outline-primary"
         className="px-5 py-2 rounded-pill fw-bold d-flex mx-auto"
